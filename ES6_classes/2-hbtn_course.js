e677dada9fdf7e2c0145b514c2720debe276004e
Basco <martinez.xabi@laposte.net>
2024-11-12 11:10:31 +0100

    Push

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,18 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (
+      !Array.isArray(students)
+      || !students.every((student) => typeof student === 'string')
+    ) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -33,7 +46,7 @@ export default class HolbertonCourse {
 
   set students(value) {
     if (
-      !Array.isArray(students)
+      !Array.isArray(value)
       || !value.every((student) => typeof student === 'string')
     ) {
       throw new TypeError('Students must be an array of strings');
