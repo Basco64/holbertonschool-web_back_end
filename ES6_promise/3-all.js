b034caa987f373de607b4fea724384f53ede4efc
Basco <martinez.xabi@laposte.net>
2024-11-19 15:36:07 +0100

    task 1,2,3,4

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  let body;
+  let firstName;
+  let lastName;
+
+  return uploadPhoto()
+    .then((data) => {
+      body = data.body;
+      createUser()
+        .then((data) => {
+          firstName = data.firstName;
+          lastName = data.lastName;
+          console.log(`${body} ${firstName} ${lastName}`);
+        })
+        .catch(() => console.log('Signup system offline'));
+    })
+    .catch(() => console.log('Signup system offline'));
+}
