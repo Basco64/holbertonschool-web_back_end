572703ea5221e771d6d5651f970ad469265d75e1
Basco <martinez.xabi@laposte.net>
2024-11-20 11:01:33 +0100

    Push

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,10 @@
-/* eslint-disable comma-dangle */
-
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(
   firstName,
   lastName,
-  fileName
+  fileName,
 ) {
   const userData = await signUpUser(firstName, lastName).then((data) => ({
     status: 'fulfilled',
