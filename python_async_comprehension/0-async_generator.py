301ddef01cdc103a6f8e18e629aca233b624e9df
Basco <martinez.xabi@laposte.net>
2024-12-09 11:08:58 +0100

    python_async_comprehension

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,6 +8,6 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """Generate 10 random numbers asynchronously."""
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.randint(1, 10)
