96e4c9703e70a139647abad5ef2228108ddf4ff3
Basco <martinez.xabi@laposte.net>
2025-01-23 10:18:42 +0100

    push

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
-function main() {
-  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-  process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}`);
-  });
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}`);
+});
 
-  process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
-
-main();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
